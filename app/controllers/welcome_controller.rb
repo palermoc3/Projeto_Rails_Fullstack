f6b9e1d4c124565ce15f31499ef7781f003b68dd
Pedro Palermo <pedropalermo97@gmai.com>
2022-04-20 16:07:23 -0300

    Security of devise started

--- app/controllers/welcome_controller.rb
+++
@@ -1,6 +0,0 @@
-class WelcomeController < ApplicationController
-#https_basic_authenticate_with name:"pedro", password:"123"
-
-  def index
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
+  namespace :site do
+    get 'welcome/index'
+  end
+  namespace :admins_backoffice do
+    get 'welcome/index'
+  end
+  namespace :users_backoffice do
+    get 'welcome/index'
+  end
   get 'welcome/index'
   devise_for :admins
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get 'login', to:"welcome#index"
+  get 'login', to:"site/welcome#index"
   # Defines the root path route ("/")
-  root to: "welcome#index"
+  root to: "site/welcome#index"
 end
